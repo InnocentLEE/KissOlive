beb7aec4ecc9d748096f9ad41be65b3b0eb3a729
InnocentLEE <13822866200@163.com>
2018-04-16 13:56:27 +0800

    后台菜单栏的数据接口

---
+++ src/kissolive/web/servlet/AdminServlet.java
@@ -0,0 +1,108 @@
+package kissolive.web.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import cn.itcast.servlet.BaseServlet;
+
+public class AdminServlet extends BaseServlet {
+	/**
+	 * 首页管理
+	 * @param req
+	 * @param resp
+	 * @return
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	public String adminHomePage(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		return "f:/";
+	}
+	/**
+	 * 选购热点管理
+	 * @param req
+	 * @param resp
+	 * @return
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	public String adminHotspot(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		return "f:/";
+	}
+	/**
+	 * 色号管理
+	 * @param req
+	 * @param resp
+	 * @return
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	public String adminColorno(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		return "f:/";
+	}
+	/**
+	 * 品牌管理
+	 * @param req
+	 * @param resp
+	 * @return
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	public String adminBrand(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		return "f:/";
+	}
+	/**
+	 * 系列管理
+	 * @param req
+	 * @param resp
+	 * @return
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	public String adminSeries(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		return "f:/";
+	}
+	/**
+	 * 口红管理
+	 * @param req
+	 * @param resp
+	 * @return
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	public String adminLipstick(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		return "f:/";
+	}
+	/**
+	 * 商品管理
+	 * @param req
+	 * @param resp
+	 * @return
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	public String adminGoods(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		return "f:/";
+	}
+	/**
+	 * 订单管理
+	 * @param req
+	 * @param resp
+	 * @return
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	public String adminOrder(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		return "f:/";
+	}
+}
