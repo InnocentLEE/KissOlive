3a907ee2915e2b040ed6be15ac0bbef08cad9241
Erica <1979322310@qq.com>
2018-04-21 22:59:43 +0800

    购物车及支付与物流及支付成功页面已完成

---
+++ WebRoot/js/shoppingcart.js
@@ -0,0 +1,249 @@
+$(function () {
+
+    //全局的checkbox选中和未选中的样式
+    var $allCheckbox = $('input[type="checkbox"]'),     //全局的全部checkbox
+        $wholeChexbox = $('.whole_check');
+        //$cartBox = $('.cartBox'),                       //每个商铺盒子
+        //$shopCheckbox = $('.shopChoice'),               //每个商铺的checkbox
+         $sonCheckBox = $('.son_check');                 //每个商铺下的商品的checkbox
+        // $("i.far.fa-check-square").hide();
+    
+    //===============================================全局全选与单个商品的关系================================
+      $wholeChexbox.click(function () {
+    /*	var txt1="<i class=\"far fa-square\"></i>";
+    	var txt2="<i class=\"far fa-check-square\"></i>";*/
+    	
+        if ($(this).is(':checked')) {
+        	$sonCheckBox.each(function() {
+        		$(this).prop("checked", true);
+        	});
+        } else {
+        	$sonCheckBox.each(function() {
+            $(this).prop("checked", false);
+        	});
+        }
+        totalMoney();
+    });
+      
+      $sonCheckBox.each(function () {
+          $(this).click(function () {
+              if ($(this).is(':checked')) {
+                  //判断：所有单个商品是否勾选
+                  var len = $sonCheckBox.length;
+                  var num = 0;
+                  $sonCheckBox.each(function () {
+                      if ($(this).is(':checked')) {
+                          num++;
+                      }
+                  });
+                  if (num == len) {
+                      $wholeChexbox.prop("checked", true);
+                  }
+              } else {
+                  //单个商品取消勾选，全局全选取消勾选
+                  $wholeChexbox.prop("checked", false);
+                  
+              }
+              totalMoney();
+          })
+          
+      });
+
+    //=================================================商品数量==============================================
+    var $plus = $('.plus'),
+        $reduce = $('.reduce'),
+        $all_sum = $('.sum');
+    $plus.click(function () {
+        var $inputVal = $(this).prev('input'),
+            $count = parseInt($inputVal.val())+1,
+            $obj = $(this).parents('.amount_box').find('.reduce'),
+            $priceTotalObj = $(this).parents('.order_lists').find('.sum_price'),
+            $price = $(this).parents('.order_lists').find('.price').html(),  //单价
+            $priceTotal = $count*parseInt($price.substring(1));
+            $gid =$("input[type='hidden']").attr("value");
+            $inputVal.val($count);
+	       // console.log($count);
+           
+            //数量变化ajax
+	       /* $.ajax({
+	    		url:"/KissOlive/MainServlet",//要请求的servlet
+	    		data:{method:"ajaxAddCart", number:$count , gid:$gid},//给服务器的参数
+	    		type:"POST",
+	    		dataType:"json",
+	    		async:false,//是否异步请求，如果是异步，那么不会等服务器返回，这个函数就向下运行了。
+	    		cache:false,
+	    		success:function(result) {
+	    			if(!result) {//如果校验失败
+	    				alert("数量增加失败！请重试！");
+	    				return false;
+	    			}
+	    		}
+	    	});*/
+        
+        $priceTotalObj.html('￥'+$priceTotal);
+        if($inputVal.val()>1 && $obj.hasClass('reSty')){
+            $obj.removeClass('reSty');
+        }
+        totalMoney();
+    });
+
+    $reduce.click(function () {
+        var $inputVal = $(this).next('input'),
+            $count = parseInt($inputVal.val())-1,
+            $priceTotalObj = $(this).parents('.order_lists').find('.sum_price'),
+            $price = $(this).parents('.order_lists').find('.price').html(),  //单价
+            $priceTotal = $count*parseInt($price.substring(1));
+            $gid =$("input[type='hidden']").attr("value");
+           
+        if($inputVal.val()>1){
+            $inputVal.val($count);
+           // console.log($count);
+           
+            //数量变化ajax
+            /*$.ajax({
+	    		url:"/KissOlive/MainServlet",//要请求的servlet
+	    		data:{method:"ajaxAddCart", number:$count , gid:$gid},//给服务器的参数
+	    		type:"POST",
+	    		dataType:"json",
+	    		async:false,//是否异步请求，如果是异步，那么不会等服务器返回，这个函数就向下运行了。
+	    		cache:false,
+	    		success:function(result) {
+	    			if(!result) {//如果校验失败
+	    				alert("数量减少失败！请重试！");
+	    				return false;
+	    			}
+	    		}
+	    	});*/
+            
+            $priceTotalObj.html('￥'+$priceTotal);
+        }
+        if($inputVal.val()==1 && !$(this).hasClass('reSty')){
+            $(this).addClass('reSty');
+        }
+        totalMoney();
+    });
+
+    $all_sum.keyup(function () {
+        var $count = 0,
+            $priceTotalObj = $(this).parents('.order_lists').find('.sum_price'),
+            $price = $(this).parents('.order_lists').find('.price').html(),  //单价
+            $priceTotal = 0;
+        if($(this).val()==''){
+            $(this).val('1');
+        }
+        $(this).val($(this).val().replace(/\D|^0/g,''));
+        $count = $(this).val();
+        $priceTotal = $count*parseInt($price.substring(1));
+        $(this).attr('value',$count);
+        $priceTotalObj.html('￥'+$priceTotal);
+        totalMoney();
+    })
+
+    //======================================总计==========================================
+
+    function totalMoney() {
+        var total_money = 0;
+        var total_count = 0;
+        var calBtn = $('.calBtn a');
+        $sonCheckBox.each(function () {
+            if ($(this).is(':checked')) {
+                var goods = parseInt($(this).parents('.order_lists').find('.sum_price').html().substring(1));
+                var num =  parseInt($(this).parents('.order_lists').find('.sum').val());
+                total_money += goods;
+                total_count += num;
+            }
+        });
+        $('.total_text').html('￥'+total_money);
+        $('.piece_num').html(total_count);
+
+        // console.log(total_money,total_count);
+
+       /* if(total_money!=0 && total_count!=0){
+            if(!calBtn.hasClass('btn_sty')){
+                calBtn.addClass('btn_sty');
+            }
+        }else{
+            if(calBtn.hasClass('btn_sty')){
+                calBtn.removeClass('btn_sty');
+            }
+        }*/
+    }
+});
+$(document).ready(function(){
+	
+	//点击立即购买触发函数
+	$(".calBtn").click(function (){
+		var judge = 0;
+    	$(".son_check").each(function() { //遍历每个元素
+    		if($('.son_check').is(':checked')) {
+    		    // 如果该商品被选中
+    			judge = 1;
+    			var cid = $(this).attr("id");
+    			
+    			 $.ajax({
+    		    		url:"/KissOlive/MainServlet",//要请求的servlet
+    		    		data:{method:"ajaxAddCart", cid:cid},//给服务器的参数
+    		    		type:"POST",
+    		    		dataType:"json",
+    		    		async:false,//是否异步请求，如果是异步，那么不会等服务器返回，这个函数就向下运行了。
+    		    		cache:false,
+    		    		success:function(result) {
+    		    			if(!result) {//如果校验失败
+    		    				alert("购买失败，请重试！！");
+    		    				return false;
+    		    			}
+    		    		
+    		    		}
+    		    	});
+    			
+    		}
+    	});
+        if(judge==0)
+        	alert("您未选中任何商品，请选择商品再点击购买！！");
+        else{
+        	$("#step1").removeClass("active in");
+			$("#step2").addClass("active in");
+			 for(var i=1;i<2;i++){
+	               $("#step"+i+"Li").addClass("gray").removeClass("active black");
+	            }
+	            for(var i=3;i<=3;i++){
+	                $("#step"+i+"Li").addClass("gray").removeClass("active black");
+	            }
+	        $("#step"+2+"Li").addClass("active black").removeClass("gray");
+	       
+        }
+    	});
+	
+	//点击支付成功触发函数
+	$(".surepay").click(function (){
+		  $('#codeModal').modal('hide');
+		  $("#step2").removeClass("active in");
+		  $("#step3").addClass("active in");
+		   for(var i=1;i<3;i++){
+	           $("#step"+i+"Li").addClass("gray").removeClass("active black");
+	        }
+	      $("#step"+3+"Li").addClass("active black").removeClass("gray");
+	   
+	});
+	
+
+});
+	    
+    /*选项卡实现流程步骤样式事件操作*/
+	   /* 
+	    var eventFun={
+	        setStep:function(index){
+	        	
+	           for(var i=1;i<index;i++){
+	               // $("#step"+i+"Li").addClass("black").removeClass("gray");
+	               $("#step"+i+"Li").addClass("gray").removeClass("black");
+	            }
+	            for(var i=index+1;i<=3;i++){
+	                $("#step"+i+"Li").addClass("gray").removeClass("black");
+	            }
+	            $("#step"+index+"Li").addClass("black").removeClass("gray");
+	        }
+	    }*/
+	    
+	  
+	   
--- WebRoot/test/modaltest.jsp
+++ WebRoot/test/modaltest.jsp
@@ -1,4 +1,5 @@
 <%@ page language="java" import="java.util.*" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%
 	String path = request.getContextPath();
 	String basePath = request.getScheme() + "://"
@@ -23,33 +24,93 @@
 	-->
 <link rel="stylesheet" type="text/css"
 	href="css/admin/bootstrap.min.css">
+<!-- <link rel="stylesheet" type="text/css" href="test/test.css"> -->
+<link rel="stylesheet" href="css/admin/fontawesome-all.css">
+
 </head>
 
 <body>
+	<table class="table table-hover table-striped">
+		<thead>
+			<tr>
+				<th><input type="checkbox" id="all" class="whole_check">
+					<label for="all" class="lcheck"></label></th>
+				<th>产品</th>
+				<th>单价</th>
+				<th>数量</th>
+				<th>价格</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr class="order_lists">
+				<th scope="row"><input type="checkbox" id="checkbox_2"
+					class="son_check"> <label for="checkbox_2"></label>
+					<div class="list_img">
+						<a href="javascript:;"><img
+							src="<c:url value='/img/user_home/6.jpg'/>" alt=""></a>
+					</div></th>
+				<td>
+				<a href="javascript:;">夏季男士迷彩无袖T恤圆领潮流韩版修身男装背心青年时尚打底衫男</a>
+				 <p class="del"><a href="javascript:;" class="delBtn">删除</a></p>
+				</td>
+				<td><p class="price">￥980</p></td>
+				<td>
+				<div class="amount_box">
+                     <a href="javascript:;" class="reduce reSty">-</a>
+                     <input type="text" value="1" class="sum">
+                     <a href="javascript:;" class="plus">+</a>
+                </div>
+				</td>
+				<td><p class="sum_price">￥980</p></td>
+			</tr>
+			<tr class="order_lists">
+				<th scope="row"><input type="checkbox" id="checkbox_3"
+					class="son_check"> <label for="checkbox_3"></label>
+					<div class="list_img">
+						<a href="javascript:;"><img
+							src="<c:url value='/img/user_home/6.jpg'/>" alt=""></a>
+					</div></th>
+				<td>
+				<a href="javascript:;">夏季男士迷彩无袖T恤圆领潮流韩版修身男装背心青年时尚打底衫男</a>
+				 <p class="del"><a href="javascript:;" class="delBtn">删除</a></p>
+				</td>
+				<td><p class="price">￥780</p></td>
+				<td>
+				<div class="amount_box">
+                     <a href="javascript:;" class="reduce reSty">-</a>
+                     <input type="text" value="1" class="sum">
+                     <a href="javascript:;" class="plus">+</a>
+                </div>
+				</td>
+				<td><p class="sum_price">￥780</p></td>
+			</tr>
+			<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td>
+			  <div class="piece">已选商品<strong class="piece_num">0</strong>件</div>
+			</td>
+			<td>
+			  <div class="totalMoney">共计: <strong class="total_text">0.00</strong></div>
+			</td>
+			</tr>
+			<tr>
+			  <td> <div class="calBtn"><a href="javascript:;">结算</a></div></td>
+			<tr>
+	</table>
 
-	<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-	  <div class="panel panel-default">
-	    <div class="panel-heading" role="tab" id="headingOne">
-	      <h4 class="panel-title">
-	        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" 
-	            aria-controls="collapseOne">
-	           	点击预览图片&nbsp;▼
-	        </a>
-	      </h4>
-	    </div>
-	    <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
-	      <div class="panel-body">
-	        <img src="img/kissolive.png">
-	       </div>
-	    </div>
-	  </div>
-	</div> 
-	
-	
-	   
-<script src="js/jquery.js"> </script>
-      <script src="js/bootstrap.min.js"></script>
+   <div class="model_bg"></div>
+    <div class="my_model">
+        <p class="title">删除宝贝<span class="closeModel">X</span></p>
+        <p>您确认要删除该宝贝吗？</p>
+        <div class="opBtn"><a href="javascript:;" class="dialog-sure">确定</a><a href="javascript:;" class="dialog-close">关闭</a></div>
+    </div> 
 
+
+	<script src="js/jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="test/carts.js"></script>
 
 </body>
 </html>
